5cc3309bdbd02d33ceaf68d548dd03469b9048e9
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2020-05-08 19:38:01 +0300

    Test tools for command line dev

--- net-ais-forward.js
+++ net-ais-forward.js
@@ -9,7 +9,26 @@ read_data();
 setInterval(read_data, delay);
 
 //----------------------------------------------------------------------------
-// Deg to Rad
+// State Mapping
+
+let stateMapping = {
+  'motoring': 0,
+  'anchored': 1,
+  'not under command': 2,
+  'restricted manouverability': 3,
+  'constrained by draft': 4,
+  'moored': 5,
+  'aground': 6,
+  'fishing': 7,
+  'sailing': 8,
+  'hazardous material high speed': 9,
+  'hazardous material wing in ground': 10,
+  'ais-sart': 14,
+  'default': 15
+}
+
+//----------------------------------------------------------------------------
+// Rad to Deg
 function radians_to_degrees(radians)
 {
   var pi = Math.PI;
@@ -67,20 +86,52 @@ function read_data() {
             console.log("sog:", ms_to_knots(jsonContent[jsonKey].navigation.speedOverGround.value));
             console.log("cog:", radians_to_degrees(jsonContent[jsonKey].navigation.courseOverGroundTrue.value));
             console.log("navStat:", jsonContent[jsonKey].navigation.state.value);
-//            console.log("rot:", jsonContent[jsonKey].navigation.rateOfTurn.value);
-            console.log("heading:", jsonContent[jsonKey].navigation.headingTrue.value);
+            console.log("navStat_nr:", stateMapping[jsonContent[jsonKey].navigation.state.value]);
+            console.log("rot:", radians_to_degrees(jsonContent[jsonKey].navigation.rateOfTurn.value));
+            console.log("heading:", radians_to_degrees(jsonContent[jsonKey].navigation.headingTrue.value));
             console.log("destination:", jsonContent[jsonKey].navigation.destination.commonName.value);
 //            console.log("eta:", jsonContent[jsonKey].navigation.destination.eta.value);
             console.log("callSign:", jsonContent[jsonKey].communication.callsignVhf);
-            console.log("imo:", jsonContent[jsonKey].registrations.imo);
+            console.log("imo:", (jsonContent[jsonKey].registrations.imo).substring(4, 20));
             console.log("id:", jsonContent[jsonKey].design.aisShipType.value.id);
             console.log("type:", jsonContent[jsonKey].design.aisShipType.value.name);
+            console.log("ais class:", jsonContent[jsonKey].sensor.ais.class.value);
             console.log("draft_cur:", jsonContent[jsonKey].design.draft.value.current);
 //            console.log("draft_max:", jsonContent[jsonKey].design.draft.value.maximum);
             console.log("lenght:", jsonContent[jsonKey].design.length.value.overall);
             console.log("beam:", jsonContent[jsonKey].design.beam.value);
+            var imo_nr = (jsonContent[jsonKey].registrations.imo).substring(4, 20);
 
-            enc_msg = {
+            enc_msg_3 = {
+              aistype: 3, // class A position report
+              repeat: 0,
+              mmsi: jsonContent[jsonKey].mmsi,
+              navstatus: stateMapping[jsonContent[jsonKey].navigation.state.value],
+              sog: ms_to_knots(jsonContent[jsonKey].navigation.speedOverGround.value),
+              lon: jsonContent[jsonKey].navigation.position.value.longitude,
+              lat: jsonContent[jsonKey].navigation.position.value.latitude,
+              cog: radians_to_degrees(jsonContent[jsonKey].navigation.courseOverGroundTrue.value),
+              hdg: radians_to_degrees(jsonContent[jsonKey].navigation.headingTrue.value),
+              rot: radians_to_degrees(jsonContent[jsonKey].navigation.rateOfTurn.value)
+            }
+
+            enc_msg_5 = {
+              aistype: 5, //class A static
+              repeat: 0,
+              mmsi: jsonContent[jsonKey].mmsi,
+              imo: (jsonContent[jsonKey].registrations.imo).substring(4, 20),
+              cargo: jsonContent[jsonKey].design.aisShipType.value.id,
+              callsign: jsonContent[jsonKey].communication.callsignVhf,
+              shipname: jsonContent[jsonKey].name,
+              draught: jsonContent[jsonKey].design.draft.value.current/10,
+              destination: jsonContent[jsonKey].navigation.destination.commonName.value,
+              dimA: 0,
+              dimB: jsonContent[jsonKey].design.length.value.overall,
+              dimC: (jsonContent[jsonKey].design.beam.value)/2,
+              dimD: (jsonContent[jsonKey].design.beam.value)/2
+            }
+
+            enc_msg_18 = {
               aistype: 18, // class B position report
               repeat: 0,
               mmsi: jsonContent[jsonKey].mmsi,
@@ -89,15 +140,40 @@ function read_data() {
               lon: jsonContent[jsonKey].navigation.position.value.longitude,
               lat: jsonContent[jsonKey].navigation.position.value.latitude,
               cog: radians_to_degrees(jsonContent[jsonKey].navigation.courseOverGroundTrue.value),
-              hdg: jsonContent[jsonKey].navigation.headingTrue.value,
+              hdg: radians_to_degrees(jsonContent[jsonKey].navigation.headingTrue.value)
             }
-            var enc = new AisEncode(enc_msg)
-            var sentence = enc.nmea
-            if ( sentence && sentence.length > 0 )
-            {
-              console.log("sending: " + sentence)
-              //app.emit('nmea0183out', sentence)
+
+            enc_msg_24_0 = {
+              aistype: 24, // class B static
+              repeat: 0,
+              part: 0,
+              mmsi: jsonContent[jsonKey].mmsi,
+              shipname: jsonContent[jsonKey].name
             }
+
+            enc_msg_24_1 = {
+              aistype: 24, // class B static
+              repeat: 0,
+              part: 1,
+              mmsi: jsonContent[jsonKey].mmsi,
+              cargo: jsonContent[jsonKey].design.aisShipType.value.id,
+              callsign: jsonContent[jsonKey].communication.callsignVhf,
+              dimA: 0,
+              dimB: jsonContent[jsonKey].design.length.value.overall,
+              dimC: (jsonContent[jsonKey].design.beam.value)/2,
+              dimD: (jsonContent[jsonKey].design.beam.value)/2
+            }
+
+            if (jsonContent[jsonKey].sensor.ais.class.value == "A") {
+               ais_out(enc_msg_3);
+               ais_out(enc_msg_5);
+            }
+            if (jsonContent[jsonKey].sensor.ais.class.value == "B") {
+               ais_out(enc_msg_18);
+               ais_out(enc_msg_24_0);
+               ais_out(enc_msg_24_1);
+            }
+
             console.log("---- AIS info from: "+ i +" ----");
             console.log("");
 
@@ -109,4 +185,15 @@ function read_data() {
 
         })
         .catch(err => console.error(err));;
+
+function ais_out(enc_msg) {
+  var enc= new AisEncode(enc_msg)
+  var sentence = enc.nmea
+  if ( sentence && sentence.length > 0 )
+  {
+    console.log(sentence)
+    //app.emit('nmea0183out', sentence)
+  }
+}
+
 }
