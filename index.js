c8f51b50cc418ba4aca71378e487e73b92478de7
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2024-01-03 17:02:28 +0200

    revert 1st commit

--- index.js
+++ index.js
@@ -307,7 +307,7 @@ module.exports = function createPlugin(app) {
             }
 
             const a = { lat: ownLat, lon: ownLon };
-            const b = { lat: lat, lon: lon };
+            const b = {  lat, lon  };
             const dist = (haversine(a, b) / 1000).toFixed(2);
 
             if (dist <= distance) {
